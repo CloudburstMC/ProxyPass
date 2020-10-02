c9a60a2b30b11a66d3af53e3e64b33184d5ff6e7
Serhat G <serhatguler@hotmail.de>
2020-10-02 12:46:57 +0200

    Update Configuration.java

--- src/main/java/com/nukkitx/proxypass/Configuration.java
+++ src/main/java/com/nukkitx/proxypass/Configuration.java
@@ -27,6 +27,8 @@ public class Configuration {
     private boolean packetTesting = false;
     @JsonProperty("log-packets")
     private boolean loggingPackets = false;
+    @JsonProperty("max-clients")
+    private Integer maxClients = -1;
     @JsonProperty("log-to")
     private LogTo logTo = LogTo.FILE;
 
