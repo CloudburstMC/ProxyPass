c68d6210ee78b8d9573550e2482cf222f72c3603
basaigh <53559772+basaigh@users.noreply.github.com>
2023-07-01 20:15:09 +0100

    Update to support recent jose4j refactor in Protocol.

--- src/main/java/org/cloudburstmc/proxypass/network/bedrock/util/ForgeryUtils.java
+++ src/main/java/org/cloudburstmc/proxypass/network/bedrock/util/ForgeryUtils.java
@@ -1,18 +1,15 @@
 package org.cloudburstmc.proxypass.network.bedrock.util;
 
-import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.shaded.json.JSONObject;
-import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.SignedJWT;
 import lombok.experimental.UtilityClass;
-import org.cloudburstmc.protocol.bedrock.util.EncryptionUtils;
+import org.jose4j.json.internal.json_simple.JSONObject;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.NumericDate;
+import org.jose4j.jwx.HeaderParameterNames;
+import org.jose4j.lang.JoseException;
 
 import java.net.URI;
 import java.security.KeyPair;
-import java.security.interfaces.ECPrivateKey;
-import java.text.ParseException;
 import java.util.Base64;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -20,51 +17,48 @@ import java.util.concurrent.TimeUnit;
 @UtilityClass
 public class ForgeryUtils {
 
-    public static SignedJWT forgeAuthData(KeyPair pair, JSONObject extraData) {
+    public static String forgeAuthData(KeyPair pair, JSONObject extraData) {
         String publicKeyBase64 = Base64.getEncoder().encodeToString(pair.getPublic().getEncoded());
-        URI x5u = URI.create(publicKeyBase64);
-
-        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.ES384).x509CertURL(x5u).build();
 
         long timestamp = System.currentTimeMillis();
         Date nbf = new Date(timestamp - TimeUnit.SECONDS.toMillis(1));
         Date exp = new Date(timestamp + TimeUnit.DAYS.toMillis(1));
 
-        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-                .notBeforeTime(nbf)
-                .expirationTime(exp)
-                .issueTime(exp)
-                .issuer("self")
-                .claim("certificateAuthority", true)
-                .claim("extraData", extraData)
-                .claim("identityPublicKey", publicKeyBase64)
-                .build();
+        JwtClaims claimsSet = new JwtClaims();
+        claimsSet.setNotBefore(NumericDate.fromMilliseconds(nbf.getTime()));
+        claimsSet.setExpirationTime(NumericDate.fromMilliseconds(exp.getTime()));
+        claimsSet.setIssuedAt(NumericDate.fromMilliseconds(exp.getTime()));
+        claimsSet.setIssuer("self");
+        claimsSet.setClaim("certificateAuthority", true);
+        claimsSet.setClaim("extraData", extraData);
+        claimsSet.setClaim("identityPublicKey", publicKeyBase64);
 
-        SignedJWT jwt = new SignedJWT(header, claimsSet);
+        JsonWebSignature jws = new JsonWebSignature();
+        jws.setPayload(claimsSet.toJson());
+        jws.setKey(pair.getPrivate());
+        jws.setAlgorithmHeaderValue("ES384");
+        jws.setHeader(HeaderParameterNames.X509_URL, publicKeyBase64);
 
         try {
-            EncryptionUtils.signJwt(jwt, (ECPrivateKey) pair.getPrivate());
-        } catch (JOSEException e) {
+            return jws.getCompactSerialization();
+        } catch (JoseException e) {
             throw new RuntimeException(e);
         }
-
-        return jwt;
     }
 
-    public static SignedJWT forgeSkinData(KeyPair pair, JSONObject skinData) {
-        URI x5u = URI.create(Base64.getEncoder().encodeToString(pair.getPublic().getEncoded()));
+    public static String forgeSkinData(KeyPair pair, JSONObject skinData) {
+        String publicKeyBase64 = Base64.getEncoder().encodeToString(pair.getPublic().getEncoded());
 
-        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.ES384).x509CertURL(x5u).build();
-
-        SignedJWT jws;
+        JsonWebSignature jws = new JsonWebSignature();
+        jws.setAlgorithmHeaderValue("ES384");
+        jws.setHeader(HeaderParameterNames.X509_URL, publicKeyBase64);
+        jws.setPayload(skinData.toJSONString());
+        jws.setKey(pair.getPrivate());
 
         try {
-            jws = new SignedJWT(header, JWTClaimsSet.parse(skinData));
-            EncryptionUtils.signJwt(jws, (ECPrivateKey) pair.getPrivate());
-        } catch (JOSEException | ParseException e) {
+            return jws.getCompactSerialization();
+        } catch (JoseException e) {
             throw new RuntimeException(e);
         }
-
-        return jws;
     }
 }
