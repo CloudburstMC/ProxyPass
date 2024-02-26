e05dc8a7b19416cd7b7bc8aeaa713e6097a57a01
Alemiz <alex@mizerak.eu>
2024-02-26 20:32:31 +0100

    Send original packets

--- src/main/java/org/cloudburstmc/proxypass/network/bedrock/session/ProxyClientSession.java
+++ src/main/java/org/cloudburstmc/proxypass/network/bedrock/session/ProxyClientSession.java
@@ -1,5 +1,6 @@
 package org.cloudburstmc.proxypass.network.bedrock.session;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.util.ReferenceCountUtil;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import org.cloudburstmc.protocol.bedrock.BedrockPeer;
 import org.cloudburstmc.protocol.bedrock.BedrockSession;
 import org.cloudburstmc.protocol.bedrock.netty.BedrockPacketWrapper;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket;
+import org.cloudburstmc.protocol.bedrock.packet.UnknownPacket;
 import org.cloudburstmc.protocol.common.PacketSignal;
 import org.cloudburstmc.proxypass.ProxyPass;
 import org.cloudburstmc.proxypass.network.bedrock.util.TestUtils;
@@ -41,7 +43,16 @@ public class ProxyClientSession extends BedrockClientSession implements ProxySes
         if (this.packetHandler == null) {
             log.warn("Received packet without a packet handler for {}:{}: {}", new Object[]{this.getSocketAddress(), this.subClientId, packet});
         } else if (this.packetHandler.handlePacket(packet) == PacketSignal.UNHANDLED && this.sendSession != null) {
-            this.sendSession.sendPacket(ReferenceCountUtil.retain(packet));
+            // this.sendSession.sendPacket(ReferenceCountUtil.retain(packet));
+
+            ByteBuf buffer = wrapper.getPacketBuffer()
+                    .retainedSlice()
+                    .skipBytes(wrapper.getHeaderLength());
+
+            UnknownPacket sendPacket = new UnknownPacket();
+            sendPacket.setPayload(buffer);
+            sendPacket.setPacketId(wrapper.getPacketId());
+            this.sendSession.sendPacket(sendPacket);
         }
     }
 }
